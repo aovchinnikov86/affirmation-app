d3095a19f35635b81a7968539c90cf5b9f5ff600
antonovcinnikov <ovchinnikov.a.s@icloud.com>
2024-05-14 14:16:04 +0300

    Initial Commit

---
+++ AffirmationApp/AffirmationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationAppApp.swift
+//  AffirmationApp
+//
+//  Created by Антон Овчинников on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
